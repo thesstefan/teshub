45272a68cb291582d0d0bad0a267b6909313da01
Stefan Stefanache <stefanstefanache@protonmail.com>
2023-03-19 16:38:34 +0200

    change WebcamStream to also contain image_paths and use string ids. add new values for WebcamStatus

--- teshub/webcam/webcam_stream.py
+++ teshub/webcam/webcam_stream.py
@@ -6,6 +6,9 @@ from typing import List
 class WebcamStatus(Enum):
     NONE: str = "NONE"
     DOWNLOADED: str = "DOWNLOADED"
+    TASK_CREATED: str = "TASK_CREATED"
+    PARTIALLY_ADNOTATED: str = "PARTIALLY_ADNOTATED"
+    ADNOTATED: str = "ADNOTATED"
     VERIFIED: str = "VERIFIED"
     RUN_INFERENCE: str = "RUN_INFERENCE"
     VERIFIED_INFERENCE: str = "VERIFIED_WITH_INFERENCE"
@@ -23,8 +26,9 @@ class WebcamLocation:
 
 @dataclass
 class WebcamStream:
-    id: int
+    id: str
     categories: List[str]
     location: WebcamLocation
     status: WebcamStatus
     image_urls: List[str] = field(init=False, default_factory=list)
+    image_paths: List[str] = field(init=False, default_factory=list)
