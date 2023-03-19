be9e4107a6a4397f496fdc927fde0a51b98c7464
Stefan Stefanache <stefanstefanache@protonmail.com>
2023-03-19 18:42:22 +0200

    add CVAT script for creating adnotation tasks

---
+++ teshub/cvat/webcam_cvat_task_creator.py
@@ -0,0 +1,46 @@
+import logging
+from dataclasses import dataclass
+
+from teshub.cvat.cvat_task_creator import CVATTaskCreator
+from teshub.dataset.webcam_dataset import WebcamDataset
+from teshub.webcam.webcam_stream import WebcamStatus
+
+
+@dataclass
+class WebcamAdnotationTaskCreator:
+    task_creator: CVATTaskCreator
+    webcam_dataset: WebcamDataset
+
+    def create_tasks(
+        self,
+        task_count: int,
+        project_id: int,
+        owner_id: int,
+        assignee_id: int,
+    ):
+        webcams = self.webcam_dataset.get_webcams_with_status(
+            WebcamStatus.DOWNLOADED, task_count
+        )
+
+        for webcam in webcams:
+            try:
+                self.task_creator.create_task(
+                    webcam.id,
+                    project_id,
+                    owner_id,
+                    assignee_id,
+                    webcam.image_paths,
+                )
+            except exceptions.ApiException as e:
+                logging.error(e)
+
+                continue
+
+            self.webcam_dataset.webcam_csv.update_record_status(
+                webcam.id, WebcamStatus.TASK_CREATED, persist=True
+            )
+
+            logging.info(
+                f"Successfully created task {webcam.id} "
+                f"with {len(webcam.image_paths)} images\n"
+            )
