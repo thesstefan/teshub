fec1c1bb37657db7f83a47b6a661ee37074cb737
Stefan Stefanache <stefanstefanache@protonmail.com>
2023-05-01 01:50:29 +0300

    add get_by_iloc in dataset/CSV manager. add extra fields to WebcamFrame to allow image loading

--- teshub/webcam/webcam_frame.py
+++ teshub/webcam/webcam_frame.py
@@ -1,6 +1,9 @@
+import os
 from dataclasses import dataclass
 from enum import Enum
 from typing import Optional
+
+from PIL import Image
 
 
 class WebcamFrameStatus(str, Enum):
@@ -19,18 +22,33 @@ class WebcamFrame:
     file_name: str
     status: WebcamFrameStatus = WebcamFrameStatus.NONE
 
+    webcam_id: Optional[str] = None
+
     labels: Optional[dict[str, float]] = None
     segmentation_path: Optional[str] = None
 
     url: Optional[str] = None
+    load_dir: Optional[str] = None
 
     @property
-    def image(self) -> None:
-        raise NotImplementedError
+    def image(self) -> Image.Image:
+        if not self.load_dir:
+            raise RuntimeError(
+                f"Image not available for frame {self.file_name}"
+            )
+
+        return Image.open(os.path.join(self.load_dir, self.file_name))
 
     @property
-    def segmentation(self) -> None:
-        raise NotImplementedError
+    def segmentation(self) -> Image.Image:
+        if not self.load_dir or not self.segmentation_path:
+            raise RuntimeError(
+                f"Segmentation not available for frame {self.file_name}"
+            )
+
+        return Image.open(
+            os.path.join(self.load_dir, self.segmentation_path)
+        )
 
     def __str__(self) -> str:
         return f"WebcamFrame(file_name={self.file_name}, status={self.status})"
