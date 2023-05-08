a27d45aaa7190bc4604317c4988334eba91af925
Stefan Stefanache <stefanstefanache@protonmail.com>
2023-05-08 23:23:22 +0300

    update server for WeatherInFormer changes

--- teshub/server/app/ml_models.py
+++ teshub/server/app/ml_models.py
@@ -1,10 +1,10 @@
 from teshub.server.config import Config
-from teshub.segmentation.predictor import SegmentationPredictor
+from teshub.recognition.predictor import WeatherInFormerPredictor
 
 config = Config()
 
 assert config.SEG_MODEL_CKPT
 
-seg_predictor = SegmentationPredictor(
+weather_informer_predictor = WeatherInFormerPredictor(
     model_checkpoint_path=config.SEG_MODEL_CKPT,
 )
