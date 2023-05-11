47babf980f6a71818226b684021229c6e36824f3
Stefan Stefanache <stefanstefanache@protonmail.com>
2023-05-11 22:58:24 +0300

    add trainer for Teshub model. shape changes in weather2weather.py

--- teshub/cli/translation_cli.py
+++ teshub/cli/translation_cli.py
@@ -6,7 +6,8 @@ from dataclasses import dataclass
 
 from teshub.dataset.webcam_dataset import WebcamDataset
 from teshub.translation.weather2weather import Weather2WeatherDataset
-from teshub.webcam.webcam_frame import WebcamFrame
+from teshub.webcam.webcam_frame import WebcamFrame, WebcamFrameStatus
+from teshub.translation.trainer import TeshubTrainer
 
 parser = argparse.ArgumentParser(
     prog="teshub_translation",
@@ -51,9 +52,18 @@ def main() -> None:
 
     def is_cloudy(frame: WebcamFrame) -> bool:
         assert frame.labels
+
+        if frame.status not in [WebcamFrameStatus.MANUALLY_ANNOTATED,
+                                WebcamFrameStatus.AUTOMATICALLY_ANNOTATED]:
+            return False
+
         return frame.labels['cloudy'] >= 0.4
 
     def is_clear(frame: WebcamFrame) -> bool:
+        if frame.status not in [WebcamFrameStatus.MANUALLY_ANNOTATED,
+                                WebcamFrameStatus.AUTOMATICALLY_ANNOTATED]:
+            return False
+
         assert frame.labels
         return frame.labels['cloudy'] < 0.4
 
@@ -62,7 +72,19 @@ def main() -> None:
         select_source=is_clear,
         select_target=is_cloudy
     )
-    print(weather2weather[0][0].shape)
+
+    trainer = TeshubTrainer(
+        weather2weather,
+        lr=0.0001,
+        lambda_recon=200,
+        batch_size=4,
+        metrics_interval=50,
+        train_val_split_ratio=0.8,
+        tb_log_dir="tb_logs"
+    )
+
+    trainer.fit()
+
 
 
 if __name__ == '__main__':
