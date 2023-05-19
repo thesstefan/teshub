c9cf91d1cc30aeab811f5d41cbdaa8c681563a0c
Stefan Stefanache <stefanstefanache@protonmail.com>
2023-05-19 16:07:46 +0300

    change values in Clear2CloudyConfig

--- teshub/translation/config/clear2cloudy_config.py
+++ teshub/translation/config/clear2cloudy_config.py
@@ -12,21 +12,32 @@ from teshub.webcam.webcam_frame import WebcamFrame, WebcamFrameStatus
 class Clear2CloudyConfig(BaseTranslationConfig):
     @staticmethod
     def attention_map(seg: torch.Tensor) -> torch.Tensor:
-        attention = torch.full_like(seg, 0.05, dtype=torch.float32)
+        attention = torch.full_like(seg, 0.075, dtype=torch.float32)
 
-        attention[
-            seg == DEFAULT_SEG_LABEL2ID['blue_sky']
-        ] = 1.0
+        attention = torch.where(
+            torch.logical_or(
+                seg == DEFAULT_SEG_LABEL2ID['blue_sky'],
+                seg == DEFAULT_SEG_LABEL2ID['fog']
+            ), 1.0, attention)
 
         attention = torch.where(
             torch.logical_or(
                 seg == DEFAULT_SEG_LABEL2ID['gray_sky'],
                 seg == DEFAULT_SEG_LABEL2ID['white_sky']
+            ), 1.0, attention)
+
+        attention = torch.where(
+            torch.logical_or(
+                seg == DEFAULT_SEG_LABEL2ID['white_clouds'],
+                seg == DEFAULT_SEG_LABEL2ID['black_clouds']
             ), 0.75, attention)
+
+        attention = torch.where(
+            seg == DEFAULT_SEG_LABEL2ID['shadow'], 0.15, attention)
 
         return attention
 
-    @staticmethod
+    @ staticmethod
     def frame_selectors() -> tuple[
         Callable[[WebcamFrame], bool],
         Callable[[WebcamFrame], bool]
